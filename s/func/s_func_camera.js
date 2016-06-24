4d28fc849bfa8e7ef19ebac1e834bde60abbb215
srnpr <srnpr@macpro>
2016-06-24 23:49:48 +0800

    a

--- s/func/s_func_camera.js
+++ s/func/s_func_camera.js
@@ -19,8 +19,8 @@ const options = {
   aspectY: 1, // android only - aspectX:aspectY, the cropping image's ratio of width to height
   quality: 1, // 0 to 1, photos only
   angle: 0, // android only, photos only
-  allowsEditing: false, // Built in functionality to resize/reposition the image after selection
-  noData: false, // photos only - disables the base64 `data` field from being generated (greatly improves performance on large photos)
+  allowsEditing: true, // Built in functionality to resize/reposition the image after selection
+  noData: true, // photos only - disables the base64 `data` field from being generated (greatly improves performance on large photos)
   storageOptions: { // if this key is provided, the image will get saved in the documents directory on ios, and the pictures directory on android (rather than a temporary directory)
     skipBackup: true, // ios only - image will NOT be backed up to icloud
     path: 'images' // ios only - will save image at /Documents/images rather than the root

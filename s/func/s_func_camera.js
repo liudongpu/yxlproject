efb229ffac16c94d5409b14e39b01695c86757c5
srnpr <srnpr@macpro>
2016-06-25 22:16:04 +0800

    a

--- s/func/s_func_camera.js
+++ s/func/s_func_camera.js
@@ -11,20 +11,17 @@ const options = {
   chooseFromLibraryButtonTitle: SFuncTop.topLangBase().upLang('camera_lib'), // specify null or empty string to remove this button
   cameraType: 'back', // 'front' or 'back'
   mediaType: 'photo', // 'photo' or 'video'
-  videoQuality: 'high', // 'low', 'medium', or 'high'
+  videoQuality: 'medium', // 'low', 'medium', or 'high'
   durationLimit: 10, // video recording max time in seconds
   //maxWidth: 1000, // photos only
   //maxHeight: 1000, // photos only
-  //aspectX: 2, // android only - aspectX:aspectY, the cropping image's ratio of width to height
-  //aspectY: 1, // android only - aspectX:aspectY, the cropping image's ratio of width to height
+  aspectX: 1, // android only - aspectX:aspectY, the cropping image's ratio of width to height
+  aspectY: 1, // android only - aspectX:aspectY, the cropping image's ratio of width to height
   quality: 1, // 0 to 1, photos only
   //angle: 0, // android only, photos only
   allowsEditing: true, // Built in functionality to resize/reposition the image after selection
   noData: true, // photos only - disables the base64 `data` field from being generated (greatly improves performance on large photos)
-  storageOptions: { // if this key is provided, the image will get saved in the documents directory on ios, and the pictures directory on android (rather than a temporary directory)
-    skipBackup: true, // ios only - image will NOT be backed up to icloud
-    path: 'images' // ios only - will save image at /Documents/images rather than the root
-  }
+  
 };
 
 

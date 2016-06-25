70b1a8ce3d173efa9c4dbc5a9fed39bcc2497a08
srnpr <srnpr@macpro>
2016-06-26 00:16:34 +0800

    qrcode

--- android/app/src/main/java/com/yxlproject/MainActivity.java
+++ android/app/src/main/java/com/yxlproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.yxlproject;
 
 import com.facebook.react.ReactActivity;
+import com.eguma.barcodescanner.BarcodeScanner;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -36,6 +38,8 @@ public class MainActivity extends ReactActivity {
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
+            new BarcodeScanner(),
+            new RCTCameraPackage(),
             new ImagePickerPackage()
         );
     }
--- js/page/page_test.js
+++ js/page/page_test.js
@@ -12,85 +12,52 @@ import {
     StyleSheet
 } from 'react-native';
 
-import SFuncApi from '../../s/func/s_func_api';
 
-import SFuncCamera from '../../s/func/s_func_camera';
-
-
-
-
-import ImagePicker from 'react-native-image-picker';
-const options = {
-  title: 'Select Avatar', // specify null or empty string to remove the title
-  cancelButtonTitle: 'Cancel',
-  takePhotoButtonTitle: 'Take Photo...', // specify null or empty string to remove this button
-  chooseFromLibraryButtonTitle: 'Choose from Library...', // specify null or empty string to remove this button
-  cameraType: 'back', // 'front' or 'back'
-  mediaType: 'photo', // 'photo' or 'video'
-  videoQuality: 'high', // 'low', 'medium', or 'high'
-  durationLimit: 10, // video recording max time in seconds
-  maxWidth: 1000, // photos only
-  maxHeight: 1000, // photos only
-  aspectX: 2, // android only - aspectX:aspectY, the cropping image's ratio of width to height
-  aspectY: 1, // android only - aspectX:aspectY, the cropping image's ratio of width to height
-  quality: 1, // 0 to 1, photos only
-  angle: 0, // android only, photos only
-  allowsEditing: false, // Built in functionality to resize/reposition the image after selection
-  noData: false, // photos only - disables the base64 `data` field from being generated (greatly improves performance on large photos)
-  storageOptions: { // if this key is provided, the image will get saved in the documents directory on ios, and the pictures directory on android (rather than a temporary directory)
-    skipBackup: true, // ios only - image will NOT be backed up to icloud
-    path: 'images' // ios only - will save image at /Documents/images rather than the root
-  }
-};
+import BarcodeScanner from 'react-native-barcode-scanner-universal';
 
 
 
 export default class PageTest extends Component {
 
-  constructor(props)
-  {
-    super(props);
-    this.state={url:''};
-  }
+  render() {
+    let scanArea = null;
+    if (Platform.OS === 'ios') {
+      scanArea = (
+        <View style={styles.rectangleContainer}>
+          <View style={styles.rectangle} />
+        </View>
+      )
+    }
 
-  imageLink(sFileUrl)
-  {
-    this.setState({url:sFileUrl});
-  }
-
-  render()
-  {
-  return ( <View >
-            <Text onPress={()=>{SFuncCamera.cammerOpen(this.imageLink.bind(this))}}>aaaaaaaaa</Text>
-            <Image source={{uri:this.state.url}} style={{width:100,height:100}}></Image>
-          </View>
-    );
-  }
-  cammer()
-  {
-    ImagePicker.showImagePicker(options, (response) => {
-  //console.log('Response = ', response);
-
-  if (response.didCancel) {
-    console.warn('User cancelled image picker');
-  }
-  else if (response.error) {
-    console.warn('ImagePicker Error: ', response.error);
-  }
-  else if (response.customButton) {
-    console.warn('User tapped custom button: ', response.customButton);
-  }
-  else {
-    // uri (on iOS)
-    const source = {uri: response.uri.replace('file://', ''), isStatic: true};
-    // uri (on android)
-    //const source = {uri: response.uri, isStatic: true};
-
-  SFuncApi.uploadFile(response.uri,(o)=>{console.warn(o.fileUrl);});
-    //fetch('',opt).then((response)=>
-  }
-});
+    return (
+      <BarcodeScanner
+        onBarCodeRead={(code) => console.warn(JSON.stringify(code))}
+        style={styles.camera}>
+        {scanArea}
+      </BarcodeScanner>
+    )
   }
 
 
 }
+
+
+
+const styles = StyleSheet.create({
+  camera: {
+    flex: 1
+  },
+  rectangleContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent'
+  },
+  rectangle: {
+    height: 250,
+    width: 250,
+    borderWidth: 2,
+    borderColor: '#00FF00',
+    backgroundColor: 'transparent'
+  }
+})

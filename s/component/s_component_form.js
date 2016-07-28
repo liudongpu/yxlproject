c4d8737a882efe2f34c2a082067baa5a8deef3ec
leili <leili@leilideMacBook-Air.local>
2016-07-29 00:25:46 +0800

    a

--- s/component/s_component_form.js
+++ s/component/s_component_form.js
@@ -85,12 +85,41 @@ export  class SCFormTextArea extends Component{
 
 
 export  class SCFormDate extends Component{
+
+  constructor(props)
+  {
+    super(props);
+    var {pField} = this.props;
+
+    var format='yyyy-MM-dd hh:mm:ss';
+
+    var now=new Date();
+
+    var args = {
+               "M+": now.getMonth() + 1,
+               "d+": now.getDate(),
+               "h+": now.getHours(),
+               "m+": now.getMinutes(),
+               "s+": now.getSeconds(),
+               "q+": Math.floor((now.getMonth() + 3) / 3),  //quarter
+               "S": now.getMilliseconds()
+           };
+           if (/(y+)/.test(format))
+               format = format.replace(RegExp.$1, (now.getFullYear() + "").substr(4 - RegExp.$1.length));
+           for (var i in args) {
+               var n = args[i];
+               if (new RegExp("(" + i + ")").test(format))
+                   format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? n : ("00" + n).substr(("" + n).length));
+           }
+    this.state={val:format};
+    SFuncForm.inFormValue(pField.pageUnique,pField.fieldData,format);
+  }
   render() {
 
     var {pField,pStyle} = this.props;
 
     return (
-        <TextInput onChangeText={(text) => SFuncForm.inFormValue(pField.pageUnique,pField.fieldData,text)} style={pStyle.input} placeholder={pField.fieldRemark}></TextInput>
+        <TextInput onChangeText={(text) => {this.setState({val:text});SFuncForm.inFormValue(pField.pageUnique,pField.fieldData,text)}} style={pStyle.input} placeholder={pField.fieldRemark} value={this.state.val}></TextInput>
 
     );
   }
